c429ccd6768aa687949929e773f5a9c47b876ec9
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2025-01-18 03:05:05 -0800

    player input

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,21 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Collider2D col;
-    public float speed = 5f;
+
+    private PlayerControls input;
+    private InputAction move, jump, interact, fire;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 100f;
     private bool transformed = false;
     public Sprite druid, animal;
+    private Vector2 moveDirection;
+    public LayerMask groundMask;
+
+    private bool canJump = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();        
         sr = GetComponent<SpriteRenderer>();
         col = GetComponent<Collider2D>();
+        input = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        move = input.Player.Move;
+        jump = input.Player.Jump;
+        move.Enable();
+        jump.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
     }
 
     // Start is called before the first frame update
@@ -27,38 +52,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        moveDirection = move.ReadValue<Vector2>();        
+        CheckJump();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+    }
+
+    float jumpVal;
+    private void CheckJump()
+    {
+        jumpVal = jump.ReadValue<float>();
+        if (Physics2D.Raycast(transform.position, Vector2.down, 1f, groundMask))
         {
-            rb.AddForce(Vector2.right * speed * Time.deltaTime);
-        }  
-        if(Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(Vector2.left * speed * Time.deltaTime);
-        }   
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            transformed = !transformed;
-            if(transformed)
-            {
-                sr.sprite = animal;
-                rb.gravityScale = 0;
-            }
-            else
-            {
-                sr.sprite = druid;
-                rb.gravityScale = 1;
-            }
+            rb.AddForce(Vector2.up * jumpForce * jumpVal, ForceMode2D.Impulse);
         }
-        if (transformed)
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                rb.AddForce(Vector2.up * speed * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                rb.AddForce(Vector2.down * speed * Time.deltaTime);
-            }
-        }
+        print(jumpVal);
     }
 }
