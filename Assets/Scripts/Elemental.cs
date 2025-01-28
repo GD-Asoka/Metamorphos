b3b96d197b6cacf7880240792d65a59c9ad71cf5
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2025-01-28 05:39:25 -0800

    player anim

--- Assets/Scripts/Elemental.cs
+++ Assets/Scripts/Elemental.cs
@@ -2,69 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elemental : MonoBehaviour, IExplodable
+public class Elemental : MonoBehaviour
 {
-    protected Player parent;
-    public float animTime = 1f, animSpeed = 0.1f;
-    public Sprite[] fireTransform, fireElemental;
-    protected SpriteRenderer sr;
-    public GameObject explosion;
+    //protected Player parent;
+    //public float animTime = 1f, animSpeed = 0.1f;
+    //public Sprite[] fireTransform, fireElemental;
+    //protected SpriteRenderer sr;
+    //public GameObject explosion;
 
-    private void OnEnable()
-    {
-        Player.Interacted += Explode;
-    }
-    private void OnDisable()
-    {
-        Player.Interacted -= Explode;
-    }
-    protected virtual void Awake()
-    {
-        parent = FindObjectOfType<Player>();
-        sr = GetComponent<SpriteRenderer>();
-    }
-    protected virtual void Start()
-    {
-        StartCoroutine(Transform());
-    }
-    protected virtual IEnumerator Transform()
-    {
-        float timeElapsed = 0;
-        int index = 0;
-        while (timeElapsed <= animTime)
-        {
-            sr.sprite = fireTransform[index];
-            index++;
-            if (index >= fireTransform.Length)
-                index = 0;
-            yield return new WaitForSeconds(animSpeed);
-            timeElapsed += animSpeed;
-        }
-        index = 0;
-        while (gameObject.activeSelf)
-        {
-            sr.sprite = fireElemental[index];
-            index++;
-            if (index >= fireElemental.Length)
-                index = 0;
-            yield return new WaitForSeconds(animSpeed);
-        }
-    }    
-    public void Explode()
-    {
-        gameObject.SetActive(false);
-    }
-    protected virtual void OnCollisionEnter2D(Collision2D collision)
-    {
-        ITriggerable x;
-        if (collision.gameObject.TryGetComponent(out x))
-        {
-            x.Trigger();
-            Explode();
-        }
-        else
-        {
-            Explode();
-        }
-    }
+    //private void OnEnable()
+    //{
+    //    Player.Interacted += Explode;
+    //}
+    //private void OnDisable()
+    //{
+    //    Player.Interacted -= Explode;
+    //}
+    //protected virtual void Awake()
+    //{
+    //    parent = FindObjectOfType<Player>();
+    //    sr = GetComponent<SpriteRenderer>();
+    //}
+    //protected virtual void Start()
+    //{
+    //    StartCoroutine(Transform());
+    //}
+    //protected virtual IEnumerator Transform()
+    //{
+    //    float timeElapsed = 0;
+    //    int index = 0;
+    //    while (timeElapsed <= animTime)
+    //    {
+    //        sr.sprite = fireTransform[index];
+    //        index++;
+    //        if (index >= fireTransform.Length)
+    //            index = 0;
+    //        yield return new WaitForSeconds(animSpeed);
+    //        timeElapsed += animSpeed;
+    //    }
+    //    index = 0;
+    //    while (gameObject.activeSelf)
+    //    {
+    //        sr.sprite = fireElemental[index];
+    //        index++;
+    //        if (index >= fireElemental.Length)
+    //            index = 0;
+    //        yield return new WaitForSeconds(animSpeed);
+    //    }
+    //}    
+    //public void Explode()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+    //protected virtual void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    ITriggerable x;
+    //    if (collision.gameObject.TryGetComponent(out x))
+    //    {
+    //        x.Trigger();
+    //        Explode();
+    //    }
+    //    else
+    //    {
+    //        Explode();
+    //    }
+    //}
 }
