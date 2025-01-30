9c3f1196ba0dd874e48edce67c1a46f543dc08ba
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2025-01-30 06:21:41 -0800

    VFX

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+    #region UI
+
+    #endregion
+    #region AUDIO
+
+    #endregion
+    #region SCENE MANAGEMENT
+
+    #endregion
 }
